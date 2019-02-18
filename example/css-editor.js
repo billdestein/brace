10e63f5b8474d098a9022f2bd6c495864ce9d4d2
Bill DeStein <bill_destein@intuit.com>
2019-02-18 10:33:40 -0800

    Get rid of examples directory for now.  It's causing an infinite symlink loop

--- example/css-editor.js
+++
@@ -1,7 +0,0 @@
-var ace = require('toolbox-brace');
-require('toolbox-brace/mode/css');
-require('toolbox-brace/theme/solarized_light');
-
-var editor = ace.edit('css-editor');
-editor.setTheme('ace/theme/solarized_light');
-editor.getSession().setMode('ace/mode/css');
